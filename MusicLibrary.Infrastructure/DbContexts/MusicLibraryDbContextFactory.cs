e5be05f10900b00502fb95cea3c7a44d1f682171
M'bazie Kone <mbazie89@gmail.com>
2025-12-06 20:29:55 +0100

    Updates

---
+++ MusicLibrary.Infrastructure/DbContexts/MusicLibraryDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MusicLibrary.Infrastructure.DbContexts
+{
+    public class MusicLibraryDbContextFactory : IDesignTimeDbContextFactory<MusicLibraryDbContext>
+    {
+        public MusicLibraryDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MusicLibraryDbContext>();
+
+            // Connection string USED ONLY for dotnet ef commands
+            var connectionString =
+                "Server=localhost,1433;Database=MusicLibraryDb;User Id=sa;Password=12345678aA!;TrustServerCertificate=True";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MusicLibraryDbContext(optionsBuilder.Options);
+        }
+
+    }
+}
