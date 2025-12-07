2740bdb41dbd91afa22bf6c8874d92d5a20555c2
M'bazie Kone <mbazie89@gmail.com>
2025-12-07 14:33:39 +0100

    feat(api): add file storage service for saving upload files

--- MusicLibrary.Infrastructure/Repositories/IMediaRepository.cs
+++ MusicLibrary.Infrastructure/Repositories/IMediaRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MusicLibrary.Domain.Entities;
 
 namespace MusicLibrary.Infrastructure.Repositories
 {
     public interface IMediaRepository
     {
+        Task Addsync(MediaItem item);
     }
 }
