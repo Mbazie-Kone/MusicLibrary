eb533d781734ef57c48b8b92f23a6e03b8d7a048
M'bazie Kone <mbazie89@gmail.com>
2025-12-26 11:20:05 +0100

    feat(frontend): add JWT HTTP interceptor

--- frontend/music-library-ui/src/main.ts
+++ frontend/music-library-ui/src/main.ts
@@ -2,11 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './app/core/interceptors/jwt.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([jwtInterceptor])
+    )
   ]
 }).catch((err) => console.error(err));
