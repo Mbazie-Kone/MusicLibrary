27765ae9c4db68764471df803dc05d73114ef159
M'bazie Kone <mbazie89@gmail.com>
2025-12-27 08:53:09 +0100

    feat(frontend): add upload link

--- frontend/music-library-ui/src/app/app.component.ts
+++ frontend/music-library-ui/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
-  template: `<app-navbar></app-navbar><router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
