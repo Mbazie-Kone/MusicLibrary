b791017cc73a6b13ff743e83706aa8db0d81dd44
M'bazie Kone <mbazie89@gmail.com>
2025-12-31 00:19:20 +0100

    feat(frontend-ui): route auth pages throuhg auth layout

--- frontend/music-library-ui/src/app/app.routes.ts
+++ frontend/music-library-ui/src/app/app.routes.ts
@@ -5,19 +5,31 @@ import { RegisterComponent } from './auth/pages/register/register.component';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 import { LogoutComponent } from './auth/pages/logout/logout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent },
-    {
-        path: 'media', 
-        canActivateChild: [authGuard],
+
+    // AUTH PAGES (NO NAVBAR)
+    { 
+        path: '',
+        component: AuthLayoutComponent,
         children: [
-            { path: '', component: MediaListComponent },
-            { path: 'upload', component: MediaUploadComponent }
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: '**', redirectTo: 'login' }
         ]
     },
-    { path: 'logout', component: LogoutComponent },
-    { path: '**', redirectTo: 'login' }
+
+    // APP PAGES (WITH NAVBAR)
+    {
+        path: 'media',
+        canActivateChild: [authGuard],
+        component: AppLayoutComponent,
+        children: [
+            { path: '', component: MediaListComponent },
+            { path: 'upload', component: MediaUploadComponent },
+            { path: 'logout', component: LogoutComponent }
+        ]
+    }
 ];
